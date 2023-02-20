8bcaa7e972898ca9d99a4eed749bb756b17e6f8b
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-20 16:29:29 +0000

    Added another get and started post request

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-app.get("/restaurants", async (require, response) => {
+app.get("/restaurants", async (request, response) => {
     const myRestaurants = await Restaurant.findAll();
     response.json(myRestaurants);
+})
+
+app.get("/restaurants/:id", async (request, response) => {
+    const id = request.params.id;
+    const restaurant = await Restaurant.findByPk(id);
+    response.send(restaurant);
+})
+
+app.post("/restaurants", async (request, response) => {
+    const { name, location, cuisine } = request.body;
+    const addRestaurant = await Restaurant.create({
+        name,
+        location,
+        cuisine
+    })
+    // const insert = await Restaurant.push(addRestaurant);
 })
 
 app.listen(port, () => {
